6e242091c3f134c10b9f17cf0181de463e7e6de9
T Floyd Wright <tfwright@gmail.com>
2022-01-03 18:16:22 -0700

    Init repo

---
+++ lib/phoenix_live_admin.ex
@@ -0,0 +1,18 @@
+defmodule PhoenixLiveAdmin do
+  @moduledoc """
+  Documentation for `PhoenixLiveAdmin`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhoenixLiveAdmin.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phoenix_live_admin_test.exs
@@ -0,0 +1,8 @@
+defmodule PhoenixLiveAdminTest do
+  use ExUnit.Case
+  doctest PhoenixLiveAdmin
+
+  test "greets the world" do
+    assert PhoenixLiveAdmin.hello() == :world
+  end
+end
