486982a7686b3ae2b807bc4c9d3910a4e69cade1
T Floyd Wright <tfwright@gmail.com>
2022-01-09 15:45:28 -0700

    Remove separate home template

--- lib/phoenix/live_admin/components/home.ex
+++ lib/phoenix/live_admin/components/home.ex
@@ -5,4 +5,16 @@ defmodule Phoenix.LiveAdmin.Components.Home do
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <h1 class="text-3xl md:text-5xl mb-4 font-extrabold" id="home">Phoenix LiveAdmin</h1>
+    <p class="py-2 text-xl">
+      This is your admin home page.
+      You will be able to customize this text with whatever copy you'd like to help orient admin users.
+      Here will be some instructions for doing that customization.
+    </p>
+    """
+  end
 end
