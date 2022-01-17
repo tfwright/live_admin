e2228082e3aaf36bde1e0d0cce3f564911dcd371
T Floyd Wright <tfwright@gmail.com>
2022-01-16 19:54:12 -0700

    Search form tweaks

--- lib/phoenix/live_admin/components/resource.ex
+++ lib/phoenix/live_admin/components/resource.ex
@@ -165,10 +165,10 @@ defmodule Phoenix.LiveAdmin.Components.Resource do
           <div class="resource__action">
             <div class="flex items-center justify-center">
               <div class="flex border-2 rounded-lg">
-                  <form phx-change="search">
-                    <input type="text" class="px-4 py-1 w-60 border-0" placeholder="Search..." name="query">
+                  <form phx-change="search" >
+                    <input type="text" class="px-4 py-1 w-60 border-0" placeholder="Search..." name="query" onkeydown="return event.key != 'Enter'">
                   </form>
-                  <button class="flex items-center justify-center px-4 border-l">
+                  <button phx-click="search" phx-value-query="" class="flex items-center justify-center px-2 border-l">
                     <svg class="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                       <path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
                     </svg>
