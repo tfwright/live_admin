67cd6103a6322b647360309621b69dbb77cc3587
T Floyd Wright <tfwright@gmail.com>
2022-01-20 16:13:06 -0700

    Fix pagination

--- lib/phoenix/live_admin/components/resource/index.ex
+++ lib/phoenix/live_admin/components/resource/index.ex
@@ -46,8 +46,8 @@ defmodule Phoenix.LiveAdmin.Components.Resource.Index do
         <tfoot>
           <tr>
             <td class="w-full" colspan={@resource |> fields(@config) |> Enum.count()}>
-              <%= if @page > 1, do: live_patch("Prev", to: @socket.router.__helpers__().resource_path(@socket, :list, @key, page: @page - 1), class: "resource__action--btn"), else: content_tag(:span, "Prev", class: "resource__action--disabled") %>
-              <%= if @page < (@records |> elem(1)) / 10, do: live_patch("Next", to: @socket.router.__helpers__().resource_path(@socket, :list, @key, page: @page + 1), class: "resource__action--btn"), else: content_tag(:span, "Next", class: "resource__action--disabled") %>
+              <%= if @page > 1, do: live_patch("Prev", to: route_with_params(@socket, [:list, @key], Map.put(@params, :page, @page - 1)), class: "resource__action--btn"), else: content_tag(:span, "Prev", class: "resource__action--disabled") %>
+              <%= if @page < (@records |> elem(1)) / 10, do: live_patch("Next", to: route_with_params(@socket, [:list, @key], Map.put(@params, :page, @page + 1)), class: "resource__action--btn"), else: content_tag(:span, "Next", class: "resource__action--disabled") %>
             </td>
             <td class="text-right p-2"><%= @records |> elem(1) %> total rows</td>
           </tr>
