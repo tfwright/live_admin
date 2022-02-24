e4bd2137418c48c4714635b40a03e56ea920d59d
T Floyd Wright <tfwright@gmail.com>
2022-02-23 22:58:23 -0700

    Refactor index into live component

--- lib/phoenix/live_admin/components/resource.ex
+++ lib/phoenix/live_admin/components/resource.ex
@@ -2,10 +2,8 @@ defmodule Phoenix.LiveAdmin.Components.Resource do
   use Phoenix.LiveView
   use Phoenix.HTML
 
-  import Ecto.Query
-
   import Phoenix.LiveAdmin,
-    only: [resource_title: 3, parent_associations: 1, get_config: 2, get_config: 3]
+    only: [resource_title: 3, get_config: 2, get_config: 3]
 
   alias Ecto.Changeset
   alias __MODULE__.{Form, Index}
@@ -69,7 +67,6 @@ defmodule Phoenix.LiveAdmin.Components.Resource do
       |> assign(sort: sort)
       |> assign(search: params["s"])
       |> assign_prefix(params["prefix"])
-      |> reload_list()
 
     {:noreply, socket}
   end
@@ -156,90 +153,6 @@ defmodule Phoenix.LiveAdmin.Components.Resource do
     {:noreply, socket}
   end
 
-  @impl true
-  def handle_event(
-        "delete",
-        %{"id" => id},
-        %{
-          assigns: %{
-            resource: resource,
-            key: key,
-            config: config,
-            session_id: session_id,
-            page: page
-          }
-        } = socket
-      ) do
-    socket =
-      id
-      |> get_resource!(resource, socket.assigns.prefix)
-      |> delete_resource(config, SessionStore.lookup(session_id))
-      |> case do
-        {:ok, _} ->
-          socket
-          |> put_flash(:info, "Deleted #{resource}")
-          |> push_redirect(to: route_with_params(socket, [:list, key], page: page))
-
-        {:error, _} ->
-          put_flash(socket, :error, "Could not delete #{resource}")
-      end
-
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_event("action", %{"action" => action, "id" => id}, socket) do
-    record = socket.assigns.records |> elem(0) |> Enum.find(&(to_string(&1.id) == id))
-
-    action_name = String.to_existing_atom(action)
-
-    session = SessionStore.lookup(socket.assigns.session_id)
-
-    {m, f, a} =
-      socket.assigns
-      |> get_in([:config, :actions, action_name])
-      |> case do
-        nil -> {socket.assigns.resource, action_name, []}
-        tuple when tuple_size(tuple) == 3 -> tuple
-      end
-
-    socket =
-      case apply(m, f, [record, session] ++ a) do
-        {:ok, result} ->
-          socket
-          |> put_flash(:info, "Successfully completed #{action}: #{inspect(result)}")
-          |> push_redirect(
-            to:
-              route_with_params(
-                socket,
-                [:list, socket.assigns.key],
-                Map.take(socket.assigns, [:prefix, :page])
-              )
-          )
-
-        {:error, error} ->
-          put_flash(socket, :error, "#{action} failed: #{error}")
-      end
-
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_event("search", %{"query" => query}, socket) do
-    params = %{
-      page: socket.assigns.page,
-      "sort-attr": elem(socket.assigns.sort, 1),
-      "sort-dir": elem(socket.assigns.sort, 0),
-      s: query,
-      prefix: socket.assigns.prefix
-    }
-
-    socket =
-      push_patch(socket, to: route_with_params(socket, [:list, socket.assigns.key], params))
-
-    {:noreply, socket}
-  end
-
   def render(assigns = %{loading: true}), do: ~H""
 
   @impl true
@@ -304,18 +217,19 @@ defmodule Phoenix.LiveAdmin.Components.Resource do
 
   def render("list.html", assigns) do
     ~H"""
-    <Index.render
+    <.live_component
+      module={Index}
+      id="list"
       socket={@socket}
       resources={@resources}
       resource={@resource}
       config={@config}
       key={@key}
       page={@page}
-      records={@records}
-      sort_attr={elem(@sort, 1)}
-      sort_dir={elem(@sort, 0)}
+      sort={@sort}
       search={@search}
       prefix={@prefix}
+      session_id={@session_id}
     />
     """
   end
@@ -338,36 +252,6 @@ defmodule Phoenix.LiveAdmin.Components.Resource do
           []
       end
     end)
-  end
-
-  def list(resource, config, opts \\ []) do
-    opts =
-      opts
-      |> Enum.into(%{})
-      |> Map.put_new(:page, 1)
-      |> Map.put_new(:sort, {:asc, :id})
-
-    query =
-      resource
-      |> limit(10)
-      |> offset(^((opts[:page] - 1) * 10))
-      |> order_by(^[opts[:sort]])
-      |> preload(^preloads_for_resource(resource, config))
-
-    query =
-      opts
-      |> Enum.reduce(query, fn
-        {:search, q}, query when byte_size(q) > 0 ->
-          apply_search(query, q, fields(resource, config))
-
-        _, query ->
-          query
-      end)
-
-    {
-      repo().all(query, prefix: opts[:prefix]),
-      repo().aggregate(query |> exclude(:limit) |> exclude(:offset), :count, prefix: opts[:prefix])
-    }
   end
 
   def route_with_params(socket, segments, params \\ %{}) do
@@ -456,61 +340,12 @@ defmodule Phoenix.LiveAdmin.Components.Resource do
     end
   end
 
-  defp delete_resource(record, config, session) do
-    config
-    |> get_config(:delete_with)
-    |> case do
-      nil ->
-        repo().delete(record)
-
-      {mod, func_name, args} ->
-        apply(mod, func_name, [record, session] ++ args)
-    end
-  end
-
   defp validate_resource(changeset, config, session) do
     config
     |> get_config(:validate_with)
     |> case do
       nil -> changeset
       {mod, func_name, args} -> apply(mod, func_name, [changeset, session] ++ args)
-    end
-  end
-
-  defp apply_search(query, q, fields) do
-    q
-    |> IO.inspect()
-    |> String.split(~r{[^\s]*:}, include_captures: true, trim: true)
-    |> case do
-      [q] ->
-        Enum.reduce(fields, query, fn {field_name, _, _}, query ->
-          or_where(
-            query,
-            [r],
-            ilike(fragment("CAST(? AS text)", field(r, ^field_name)), ^"%#{q}%")
-          )
-        end)
-
-      field_queries ->
-        field_queries
-        |> Enum.map(&String.trim/1)
-        |> Enum.chunk_every(2)
-        |> Enum.reduce(query, fn
-          [field_key, q], query ->
-            if {field_name, _, _} =
-                 Enum.find(fields, fn {field_name, _, _} -> "#{field_name}:" == field_key end) do
-              or_where(
-                query,
-                [r],
-                ilike(fragment("CAST(? AS text)", field(r, ^field_name)), ^"%#{q}%")
-              )
-            else
-              query
-            end
-
-          [_], query ->
-            query
-        end)
     end
   end
 
@@ -523,26 +358,5 @@ defmodule Phoenix.LiveAdmin.Components.Resource do
     SessionStore.set(socket.assigns.session_id, :__prefix__, prefix)
 
     assign(socket, :prefix, prefix)
-  end
-
-  defp reload_list(socket),
-    do:
-      assign(socket,
-        records:
-          list(
-            socket.assigns.resource,
-            socket.assigns.config,
-            Map.take(socket.assigns, [:prefix, :sort, :page, :search])
-          )
-      )
-
-  defp preloads_for_resource(resource, config) do
-    config
-    |> Map.get(:preload)
-    |> case do
-      nil -> resource |> parent_associations() |> Enum.map(& &1.field)
-      {m, f, a} -> apply(m, f, [resource | a])
-      preloads when is_list(preloads) -> preloads
-    end
   end
 end
--- lib/phoenix/live_admin/components/resource/index.ex
+++ lib/phoenix/live_admin/components/resource/index.ex
@@ -1,18 +1,35 @@
 defmodule Phoenix.LiveAdmin.Components.Resource.Index do
-  use Phoenix.Component
+  use Phoenix.LiveComponent
   use Phoenix.HTML
 
   import Phoenix.LiveAdmin.Components.Resource,
-    only: [repo: 0, fields: 2, route_with_params: 3]
+    only: [repo: 0, fields: 2, route_with_params: 3, get_resource!: 3]
 
-  import Phoenix.LiveAdmin, only: [associated_resource: 3, record_label: 2]
+  import Phoenix.LiveAdmin, only: [associated_resource: 3, record_label: 2, get_config: 2, parent_associations: 1]
 
+  import Ecto.Query
+
+  alias Phoenix.LiveAdmin.SessionStore
+
+  @impl true
   def render(assigns) do
+    assigns =
+      assign(assigns,
+        records:
+          list(
+            assigns.resource,
+            assigns.config,
+            Map.take(assigns, [:prefix, :sort, :page, :search])
+          ),
+        sort_attr: elem(assigns.sort, 1),
+        sort_dir: elem(assigns.sort, 0)
+      )
+
     ~H"""
     <div class="resource__list">
       <div class="list__search">
         <div class="flex border-2 rounded-lg">
-            <form phx-change="search" >
+            <form phx-change="search" phx-target={@myself}>
               <input
                 type="text"
                 class="px-4 py-1 w-60 border-0 h-8"
@@ -53,9 +70,9 @@ defmodule Phoenix.LiveAdmin.Components.Resource.Index do
               <% end %>
               <td class="resource__cell">
                 <%= live_redirect "Edit", to: route_with_params(@socket, [:edit, @key, record.id], prefix: @prefix), class: "resource__action--btn" %>
-                <%= link "Delete", to: "#", "data-confirm": "Are you sure?", "phx-click": "delete", "phx-value-id": record.id, class: "resource__action--btn" %>
+                <%= link "Delete", to: "#", "data-confirm": "Are you sure?", "phx-click": "delete", "phx-value-id": record.id, "phx-target": @myself, class: "resource__action--btn" %>
                 <%= for action <- Map.get(@config, :actions, []) do %>
-                  <%= link action |> to_string() |> humanize(), to: "#", "data-confirm": "Are you sure?", "phx-click": "action", "phx-value-id": record.id, "phx-value-action": action, class: "resource__action--btn" %>
+                  <%= link action |> to_string() |> humanize(), to: "#", "data-confirm": "Are you sure?", "phx-click": "action", "phx-value-id": record.id, "phx-value-action": action, "phx-target": @myself, class: "resource__action--btn" %>
                 <% end %>
               </td>
             </tr>
@@ -73,6 +90,90 @@ defmodule Phoenix.LiveAdmin.Components.Resource.Index do
       </table>
     </div>
     """
+  end
+
+  @impl true
+  def handle_event(
+        "delete",
+        %{"id" => id},
+        %{
+          assigns: %{
+            resource: resource,
+            key: key,
+            config: config,
+            session_id: session_id,
+            page: page
+          }
+        } = socket
+      ) do
+    socket =
+      id
+      |> get_resource!(resource, socket.assigns.prefix)
+      |> delete_resource(config, SessionStore.lookup(session_id))
+      |> case do
+        {:ok, _} ->
+          socket
+          |> put_flash(:info, "Deleted #{resource}")
+          |> push_redirect(to: route_with_params(socket, [:list, key], page: page))
+
+        {:error, _} ->
+          put_flash(socket, :error, "Could not delete #{resource}")
+      end
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("action", %{"action" => action, "id" => id}, socket) do
+    record = get_resource!(id, socket.assigns.resource, socket.assigns.prefix)
+
+    action_name = String.to_existing_atom(action)
+
+    session = SessionStore.lookup(socket.assigns.session_id)
+
+    {m, f, a} =
+      socket.assigns
+      |> get_in([:config, :actions, action_name])
+      |> case do
+        nil -> {socket.assigns.resource, action_name, []}
+        tuple when tuple_size(tuple) == 3 -> tuple
+      end
+
+    socket =
+      case apply(m, f, [record, session] ++ a) do
+        {:ok, result} ->
+          socket
+          |> put_flash(:info, "Successfully completed #{action}: #{inspect(result)}")
+          |> push_redirect(
+            to:
+              route_with_params(
+                socket,
+                [:list, socket.assigns.key],
+                Map.take(socket.assigns, [:prefix, :page])
+              )
+          )
+
+        {:error, error} ->
+          put_flash(socket, :error, "#{action} failed: #{error}")
+      end
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("search", %{"query" => query}, socket) do
+    params = %{
+      page: socket.assigns.page,
+      "sort-attr": elem(socket.assigns.sort, 1),
+      "sort-dir": elem(socket.assigns.sort, 0),
+      s: query,
+      prefix: socket.assigns.prefix
+    }
+
+    socket =
+      push_patch(socket, to: route_with_params(socket, [:list, socket.assigns.key], params))
+
+    {:noreply, socket}
   end
 
   def display_field(record = %resource{}, field_name, assigns) do
@@ -103,4 +204,92 @@ defmodule Phoenix.LiveAdmin.Components.Resource.Index do
   def list_link(socket, content, key, params, opts \\ []),
     do:
       live_patch(content, Keyword.put(opts, :to, route_with_params(socket, [:list, key], params)))
+
+  defp delete_resource(record, config, session) do
+    config
+    |> get_config(:delete_with)
+    |> case do
+      nil ->
+        repo().delete(record)
+
+      {mod, func_name, args} ->
+        apply(mod, func_name, [record, session] ++ args)
+    end
+  end
+
+  defp list(resource, config, opts) do
+    opts =
+      opts
+      |> Enum.into(%{})
+      |> Map.put_new(:page, 1)
+      |> Map.put_new(:sort, {:asc, :id})
+
+    query =
+      resource
+      |> limit(10)
+      |> offset(^((opts[:page] - 1) * 10))
+      |> order_by(^[opts[:sort]])
+      |> preload(^preloads_for_resource(resource, config))
+
+    query =
+      opts
+      |> Enum.reduce(query, fn
+        {:search, q}, query when byte_size(q) > 0 ->
+          apply_search(query, q, fields(resource, config))
+
+        _, query ->
+          query
+      end)
+
+    {
+      repo().all(query, prefix: opts[:prefix]),
+      repo().aggregate(query |> exclude(:limit) |> exclude(:offset), :count, prefix: opts[:prefix])
+    }
+  end
+
+  defp apply_search(query, q, fields) do
+    q
+    |> String.split(~r{[^\s]*:}, include_captures: true, trim: true)
+    |> case do
+      [q] ->
+        Enum.reduce(fields, query, fn {field_name, _, _}, query ->
+          or_where(
+            query,
+            [r],
+            ilike(fragment("CAST(? AS text)", field(r, ^field_name)), ^"%#{q}%")
+          )
+        end)
+
+      field_queries ->
+        field_queries
+        |> Enum.map(&String.trim/1)
+        |> Enum.chunk_every(2)
+        |> Enum.reduce(query, fn
+          [field_key, q], query ->
+            if {field_name, _, _} =
+                 Enum.find(fields, fn {field_name, _, _} -> "#{field_name}:" == field_key end) do
+              or_where(
+                query,
+                [r],
+                ilike(fragment("CAST(? AS text)", field(r, ^field_name)), ^"%#{q}%")
+              )
+            else
+              query
+            end
+
+          [_], query ->
+            query
+        end)
+    end
+  end
+
+  defp preloads_for_resource(resource, config) do
+    config
+    |> Map.get(:preload)
+    |> case do
+      nil -> resource |> parent_associations() |> Enum.map(& &1.field)
+      {m, f, a} -> apply(m, f, [resource | a])
+      preloads when is_list(preloads) -> preloads
+    end
+  end
 end
