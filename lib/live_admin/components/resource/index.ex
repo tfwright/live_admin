8b5f7640e3ae23dbd16caaa03d0a0b960e67f020
T Floyd Wright <tfwright@gmail.com>
2022-03-07 22:14:34 -0700

    Fix search clearing

--- lib/live_admin/components/resource/index.ex
+++ lib/live_admin/components/resource/index.ex
@@ -38,7 +38,7 @@ defmodule LiveAdmin.Components.Container.Index do
                 phx-debounce="500"
               />
             </form>
-            <button phx-click="search" phx-value-query="" class="flex items-center justify-center px-2 border-l">
+            <button phx-click="search" phx-value-query="" phx-target={@myself} class="flex items-center justify-center px-2 border-l">
               <svg class="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
               </svg>
