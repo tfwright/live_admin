f0a7bad5f517878d9713b50badda86c6f20ada4d
T Floyd Wright <tfwright@gmail.com>
2022-03-20 12:25:08 -0600

    Bump release

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LiveAdmin.MixProject do
     [
       app: :live_admin,
       description: "A admin UI for Phoenix applications built with LiveView",
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
