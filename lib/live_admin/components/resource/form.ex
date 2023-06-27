2fb36e99479a3f152810dcc4ee9be31f79bd2442
T Floyd Wright <tfwright@gmail.com>
2023-06-27 10:59:25 +0300

    Add catch-all field class

--- lib/live_admin/components/resource/form.ex
+++ lib/live_admin/components/resource/form.ex
@@ -473,6 +473,7 @@ defmodule LiveAdmin.Components.Container.Form do
   defp field_class({:array, _}), do: "array"
   defp field_class({_, Ecto.Embedded, _}), do: "embed"
   defp field_class({_, Ecto.Enum, _}), do: "enum"
+  defp field_class(_), do: "other"
 
   defp supported_type?(type) when type in @supported_primitive_types, do: true
   defp supported_type?(:map), do: true
