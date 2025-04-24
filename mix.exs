7d376feb86a98b553b3c8ba57e6bd06776d35589
Jake Johnson <jake.johnson@blockitnow.com>
2025-04-23 21:57:58 -0500

    update to live_view ~> 1.0.0

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule LiveAdmin.MixProject do
   defp deps do
     [
       {:phoenix_view, "~> 2.0"},
-      {:phoenix_live_view, ">= 0.20.0 and < 0.21.0"},
+      {:phoenix_live_view, "~> 1.0.0"},
       {:phoenix_html, "~> 4.0"},
       {:phoenix_html_helpers, "~> 1.0"},
       {:ecto, "~> 3.12"},
