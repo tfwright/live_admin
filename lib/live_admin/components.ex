f3b518c296c79549516af0afb7233508d04829d6
T Floyd Wright <tfwright@gmail.com>
2025-10-19 10:15:43 -0800

    modal

--- lib/live_admin/components.ex
+++ lib/live_admin/components.ex
@@ -18,42 +18,24 @@ defmodule LiveAdmin.Components do
 
     ~H"""
     <div id={"field-expand-" <> @id} phx-hook="CopyField">
-      <div class="modal" id={"modal-" <> @id}>
-        <div
-          class="modal-content"
-          phx-click-away={JS.hide(to: "#modal-" <> @id)}
+      <.modal id={@id}>
+        <:title>{record_label(@record, @resource, @config)}<span>{@field}</span></:title>
+        <div class="detail-section-content">{@record |> Map.fetch!(@field) |> safe_render()}</div>
+        <span
+          class="copy-icon"
+          data-clipboard-target={"#field-expand-#{@id} .detail-section-content"}
         >
-          <div class="modal-header">
-            <h3 class="modal-title">
-              {record_label(@record, @resource, @config)}
-              <span>{@field}</span>
-            </h3>
-            <button
-              class="modal-close"
-              phx-click={JS.hide(to: "#modal-" <> @id)}
-            >
-              &times;
-            </button>
-          </div>
-          <div class="modal-body">
-            <div class="detail-section-content">{@record |> Map.fetch!(@field) |> safe_render()}</div>
-            <span
-              class="copy-icon"
-              data-clipboard-target={"#field-expand-#{@id} .detail-section-content"}
-            >
-              <svg
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-              >
-                <rect x="9" y="9" width="13" height="13" rx="2" ry="2" />
-                <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" />
-              </svg>
-            </span>
-          </div>
-        </div>
-      </div>
+          <svg
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <rect x="9" y="9" width="13" height="13" rx="2" ry="2" />
+            <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" />
+          </svg>
+        </span>
+      </.modal>
       <span
         class="expand-icon"
         phx-click={JS.show(to: "#modal-" <> @id, display: "flex")}
@@ -99,39 +81,30 @@ defmodule LiveAdmin.Components do
     """
   end
 
+
   def modal(assigns) do
     ~H"""
-    <div
-      id={@id}
-      class="modal"
-      phx-capture-click={
-        JS.hide(to: "##{@id}", transition: {"ease-out duration-300", "opacity-100", "opacity-0"})
-      }
-    >
-      <div>
-        {render_slot(@inner_block)}
+    <div class="modal" id={"modal-" <> @id}>
+      <div
+        class="modal-content"
+        phx-click-away={JS.hide(to: "#modal-" <> @id)}
+      >
+        <div class="modal-header">
+          <h3 class="modal-title">
+            {render_slot(@title)}
+          </h3>
+          <button
+            class="modal-close"
+            phx-click={JS.hide(to: "#modal-" <> @id)}
+          >
+            &times;
+          </button>
+        </div>
+        <div class="modal-body">
+          {render_slot(@inner_block)}
+        </div>
       </div>
     </div>
     """
   end
-
-  defp list(assigns) do
-    ~H"""
-    <div>
-      <nav>
-        <ul>
-          <%= if Enum.empty?(@items) && assigns[:empty_label] do %>
-            <li>{render_slot(@empty_label)}</li>
-          <% end %>
-          <%= for item <- @items do %>
-            <li>{render_slot(@inner_block, item)}</li>
-          <% end %>
-        </ul>
-      </nav>
-    </div>
-    """
-  end
-
-  defp embed_fields({_, {_, %{related: schema}}}),
-    do: Enum.map(schema.__schema__(:fields), &{&1, schema.__schema__(:type, &1), []})
 end
