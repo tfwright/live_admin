9bee122410bc0b87a3a64d15940c12cc97fc62f1
T Floyd Wright <tfwright@gmail.com>
2025-10-31 11:13:04 -0800

    warnings

--- dev/renderer.ex
+++ dev/renderer.ex
@@ -1,24 +1,4 @@
 defmodule DemoWeb.Renderer do
-  use PhoenixHTMLHelpers
-
-  def render_field(record, field, _session) do
-    record
-    |> Map.fetch!(field)
-    |> case do
-      bool when is_boolean(bool) ->
-        if bool, do: "Yes", else: "No"
-      date = %Date{} ->
-        Calendar.strftime(date, "%a, %B %d %Y")
-      _ ->
-        record
-        |> Map.fetch!(field)
-        |> case do
-          val when is_binary(val) -> val
-          val -> inspect(val, pretty: true)
-        end
-    end
-  end
-
   def render_css(_) do
     __DIR__
     |> Path.join("../dist/css/default_overrides.css")
