d26607aa4a6a1560c91cc793c516396d55cba507
T Floyd Wright <tfwright@gmail.com>
2025-12-27 10:37:03 -0700

    fixup! fixup! Redesign

--- lib/live_admin/components/alert_bar.ex
+++ lib/live_admin/components/alert_bar.ex
@@ -48,7 +48,6 @@ defmodule LiveAdmin.Components.AlertBar do
                 <line x1="18" y1="6" x2="6" y2="18"></line>
                 <line x1="6" y1="6" x2="18" y2="18"></line>
               </svg>
-              <div></div>
             <% end %>
 
             <span class="alert-message">{alert}</span>
