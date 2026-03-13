7f8f8ba145e07a1bd02b0c1626198a0cd26f6857
T. Floyd Wright <tfwright@gmail.com>
2026-03-12 16:00:22 -0800

    Redesign (#132)
    * Fix config default handling
    
    * Redesign

--- dev/repo.ex
+++ dev/repo.ex
@@ -1,5 +1,5 @@
 defmodule Demo.Repo do
   use Ecto.Repo, otp_app: :live_admin, adapter: Ecto.Adapters.Postgres
 
-  def prefixes, do: ["public", "alt"]
+  def prefixes, do: ["public", "alt"] ++ Enum.map(0..100, & "fake #{&1}")
 end
